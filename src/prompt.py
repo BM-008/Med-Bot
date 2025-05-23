c88a4103da257423d79b540d75de00633915de8a
BM-008 <oneshortbm@gmail.com>
2025-05-23 11:31:18 +0530

    updated

--- src/prompt.py
+++ src/prompt.py
@@ -3,8 +3,7 @@
 
 
 system_prompt=(
-    "You are an assistant for question-answering tasks."
-    "Use the following pieces of retrieved context to answer the question. if you don't know the answer, say that you don't know."
+    "You are a helpful medical assistant. Answer the user's question directly and clearly. if you don't know the answer, say that you don't know."
     "Use three sentences maximum and keep the answer concise."
     "\n\n"
     "{context}"
